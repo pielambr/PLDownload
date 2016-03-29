a34ce653f262888c17ae92e348adb0892b74a94c
Pieterjan Lambrecht <me@pielambr.be>
2016-03-29 19:50:11 +0200

    Use hooks for progress updates

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
-import youtube_dl, os
+import os, youtube_dl
+from youtube_dl import YoutubeDL
 from multiprocessing.pool import ThreadPool
 from youtube_dl.utils import DownloadError
 from datetime import datetime
@@ -6,34 +7,49 @@ from uuid import uuid4
 
 
 class Download:
-    link = ""
+    link = ''
     done = False
     error = False
     started = None
-    uuid = ""
+    uuid = ''
     total = 0
     finished = 0
-    title = ""
+    title = ''
 
     def __init__(self, link):
         self.link = link
         self.started = datetime.now()
         self.uuid = str(uuid4())
 
+    def __call__(self, info):
+        if info['status'] == 'finished':
+            self.finished += 1
+        
+        print("\n \n INFO: " + str(info) + "\n")
+
     def download(self):
         curr_path = os.path.dirname(os.path.abspath(__file__))
-        output_path = curr_path + "/downloads/" + self.uuid + "/%(title)s-%(id)s.%(ext)s"
+        output_tmpl = curr_path + '/downloads/' + self.uuid + '/%(title)s-%(id)s.%(ext)s'
         try:
-            youtube_dl._real_main(["--yes-playlist", "-R", "10", "-x", "--audio-format", "mp3",
-                                   "--output", output_path,
-                                   "--restrict-filenames", "-v", self.link])
+            options = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '5',
+                }],
+                'progress_hooks': [self],
+                'outtmpl': output_tmpl,
+            }
+            ydl = YoutubeDL(options)
+            ydl.download([self.link])
         except DownloadError:
             self.error = True
         finally:
             self.done = True
 
     def get_files(self):
-        file_path = os.path.dirname(os.path.abspath(__file__)) + "/downloads/" + self.uuid
+        file_path = os.path.dirname(os.path.abspath(__file__)) + '/downloads/' + self.uuid
         return [f for f in os.listdir(file_path) if os.isfile(os.join(file_path, f))]
 
     def start(self):
