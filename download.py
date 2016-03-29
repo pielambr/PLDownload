b97a5c0acb058082872b1992db2d0f868c00d337
Pieterjan Lambrecht <me@pielambr.be>
2016-03-29 15:49:27 +0200

    Static serving of downloaded content

--- download.py
+++ download.py
@@ -1,7 +1,8 @@
-import youtube_dl
+import youtube_dl, os
 from multiprocessing.pool import ThreadPool
 from youtube_dl.utils import DownloadError
 from datetime import datetime
+from uuid import uuid4
 
 
 class Download:
@@ -9,18 +10,24 @@ class Download:
     done = False
     error = False
     started = None
+    uuid = ""
 
     def __init__(self, link):
         self.link = link
         self.started = datetime.now()
+        self.uuid = str(uuid4())
 
     def download(self):
-            try:
-                youtube_dl._real_main(["--yes-playlist", "-R", "10", "-x", "--audio-format", "mp3", "-v", self.link])
-            except DownloadError:
-                self.error = True
-            finally:
-                self.done = True
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        output_path = curr_path + "/downloads/" + self.uuid + "/%(title)s-%(id)s.%(ext)s"
+        try:
+            youtube_dl._real_main(["--yes-playlist", "-R", "10", "-x", "--audio-format", "mp3",
+                                   "--output", output_path,
+                                   "--restrict-filenames", "-v", self.link])
+        except DownloadError:
+            self.error = True
+        finally:
+            self.done = True
 
     def start(self):
             pool = ThreadPool()
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, send_from_directory
 from uuid import uuid4
-from urllib.request import URLError
+from os import path
 from downloader import Downloader
-from youtube_dl.utils import DownloadError
 app = Flask(__name__)
 app.secret_key = '93)q.2M)k7#X02yt,nbz"eA6EfOw9s$N_e3kh4E'
 downloader = Downloader()
@@ -11,7 +10,7 @@ downloader = Downloader()
 @app.before_request
 def before_request():
     if 'session_id' not in session:
-        session['session_id'] = uuid4()
+        session['session_id'] = str(uuid4())
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,6 +18,13 @@ def index():
     if request.method == 'POST':
         downloader.add_download(session['session_id'], request.form['pl_link'])
     return render_template("index.html", list=downloader.get_downloads(session['session_id']))
+
+
+@app.route('/download/<playlist_id>/<file>', methods=['GET'])
+def download(playlist_id, file):
+    curr_path = path.dirname(path.abspath(__file__))
+    dl_path = curr_path + '/downloads/' + playlist_id + '/'
+    return send_from_directory(dl_path, file)
 
 
 if __name__ == '__main__':
